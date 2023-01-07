040cc1531055e7e58c76a95eccc7526c178ceec6
Mayank <mayank99@users.noreply.github.com>
2023-01-07 13:16:42 -0500

    doesn't work for some reason

--- package/index.mjs
+++ package/index.mjs
@@ -1,6 +1,6 @@
+import path from 'node:path';
 import { nanoid } from 'nanoid';
-import postcss from 'postcss';
-import nested from 'postcss-nested';
+// import * as lightningCss from 'lightningcss';
 
 export function css() {
 	throw new Error(
@@ -13,25 +13,26 @@ export function vitePlugin() {
 	const cssList = new Map();
 
 	return {
-		enforce: 'pre',
+		enforce: 'post',
 		name: 'ecsstatic',
+
 		resolveId(id) {
 			if (cssList.has(id)) return id;
 			return null;
 		},
+
 		load(id) {
 			if (cssList.has(id)) {
 				return cssList.get(id);
 			}
 		},
+
 		transform(code, id) {
 			if (id.endsWith('.tsx')) {
-				let parsedAst = this.parse(code);
+				const parsedAst = this.parse(code);
 
 				const importName = getImportNameFromTree(parsedAst);
-				if (!importName) {
-					return;
-				}
+				if (!importName) return;
 
 				const cssTemplateDeclarations = parsedAst.body.filter(
 					(node) =>
@@ -39,17 +40,29 @@ export function vitePlugin() {
 						node.declarations?.[0]?.init?.type === 'TaggedTemplateExpression' &&
 						node.declarations?.[0]?.init?.tag?.name === importName
 				);
+				if (cssTemplateDeclarations?.length === 0) return;
 
 				cssTemplateDeclarations.forEach((node) => {
 					const init = node.declarations[0].init;
 					const templateContents = init.quasi.quasis[0].value.raw;
-					const [css, id] = processCss(templateContents);
+					const fileNameWithoutExt = path.parse(id).name;
 
+					const [css, className] = processCss(templateContents, fileNameWithoutExt);
+
+					// add processed css to a new or existing .css file
+					const cssFileName = `${fileNameWithoutExt}.acab.css`;
+					if (cssList.has(cssFileName)) {
+						const existingCss = cssList.get(cssFileName);
+						cssList.set(cssFileName, `${existingCss}\n${css}`);
+					} else {
+						cssList.set(cssFileName, css);
+						code = `import "${cssFileName}";\n${code}\n`; // only import this once
+					}
+
+					// replace the tagged template literal with the generated className
 					const start = init.tag.start;
 					const end = init.quasi.end;
-					code = code.replace(code.slice(start, end), `"${id}"`);
-					cssList.set(`${id}.css`, css);
-					code = `import "${id}.css";\n${code}\n`;
+					code = code.replace(code.slice(start, end), className);
 				});
 
 				return { code };
@@ -58,11 +71,23 @@ export function vitePlugin() {
 	};
 }
 
-const processCss = (templateContents = '') => {
-	const id = nanoid(8);
-	const { css } = postcss([nested()]).process(`.${id}{${templateContents}}`);
+const processCss = (templateContents = '', fileNameWithoutExt = '') => {
+	const className = nanoid(8);
+	const unprocessedCss = `.${className}{${templateContents}}`;
 
-	return [css, id];
+	// const css = lightningCss.transform({
+	// 	filename: `${fileNameWithoutExt}.acab.css`,
+	// 	code: Buffer.from(unprocessedCss),
+	// 	minify: true,
+	// 	nesting: true,
+	// 	visitor: {
+	// 		Rule(rule) {
+	// 			console.log(rule);
+	// 		},
+	// 	},
+	// });
+
+	return [unprocessedCss, className];
 };
 
 const getImportNameFromTree = (ast) => {
