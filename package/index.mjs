c31f393a2a43d1cdb156d04c6312212ad52a2b62
Mayank <mayank99@users.noreply.github.com>
2023-01-07 11:30:41 -0500

    Fix import processing

--- package/index.mjs
+++ package/index.mjs
@@ -27,32 +27,29 @@ export function vitePlugin() {
 		transform(code, id) {
 			if (id.endsWith('.tsx')) {
 				let parsedAst = this.parse(code);
-				// const cssImport = parsedAst.body.find(
-				// 	(node) => node.type === 'ImportDeclaration' && node.source.value === '@acab/ecsstatic'
-				// );
 
-				// if (cssImport) {
-				// 	const { start: s, end: e } = cssImport;
-				// }
+				const importName = getImportNameFromTree(parsedAst);
+				if (!importName) {
+					return;
+				}
 
 				const cssTemplateDeclarations = parsedAst.body.filter(
 					(node) =>
 						node.type === 'VariableDeclaration' &&
 						node.declarations?.[0]?.init?.type === 'TaggedTemplateExpression' &&
-						node.declarations?.[0]?.init?.tag?.name === 'css'
+						node.declarations?.[0]?.init?.tag?.name === importName
 				);
 
 				cssTemplateDeclarations.forEach((node) => {
-					const id = nanoid(8);
 					const init = node.declarations[0].init;
-					let css = init.quasi.quasis[0].value.raw;
-					css = postcss([nested()]).process(`.${id}{${css}}`).css;
+					const templateContents = init.quasi.quasis[0].value.raw;
+					const [css, id] = processCss(templateContents);
 
 					const start = init.tag.start;
 					const end = init.quasi.end;
 					code = code.replace(code.slice(start, end), `"${id}"`);
 					cssList.set(`${id}.css`, css);
-					code = `${code}\nimport "${id}.css";\n`;
+					code = `import "${id}.css";\n${code}\n`;
 				});
 
 				return { code };
@@ -60,3 +57,27 @@ export function vitePlugin() {
 		},
 	};
 }
+
+const processCss = (templateContents = '') => {
+	const id = nanoid(8);
+	const { css } = postcss([nested()]).process(`.${id}{${templateContents}}`);
+
+	return [css, id];
+};
+
+const getImportNameFromTree = (ast) => {
+	let importName = '';
+
+	for (const node of ast.body) {
+		if (node.type === 'ImportDeclaration') {
+			if (node.source.value === '@acab/ecsstatic') {
+				if (node.specifiers[0].imported.name === 'css') {
+					importName = node.specifiers[0].local.name;
+					break;
+				}
+			}
+		}
+	}
+
+	return importName;
+};
