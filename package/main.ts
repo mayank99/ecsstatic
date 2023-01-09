f08e69f234b58c3f6971e84188340c5029de214a
Mayank <mayank99@users.noreply.github.com>
2023-01-08 21:26:46 -0500

    resolve imports using esbuild

--- package/main.ts
+++ package/main.ts
@@ -1,3 +1,4 @@
+import esbuild from 'esbuild';
 import { createUnplugin } from 'unplugin';
 import postcssNested from 'postcss-nested';
 import hash from './hash.js';
@@ -31,21 +32,25 @@ export const plugin = createUnplugin(() => {
 			return id.endsWith('.tsx');
 		},
 
-		transform(code) {
+		transform(code, id) {
 			const parsedAst = this.parse(code) as Program;
 			const magicCode = new MagicString(code);
 
-			const [importName, importStart, importEnd] = processImport(parsedAst);
-			if (!importName) return;
+			const [ecsstaticImportName, importStart, importEnd] = processImport(parsedAst);
+			if (!ecsstaticImportName) return;
 
 			const cssTemplateDeclarations = parsedAst.body.filter(
 				(node) =>
 					node.type === 'VariableDeclaration' &&
-					node.declarations[0].init?.type === 'TaggedTemplateExpression' &&
-					node.declarations[0].init.tag.type === 'Identifier' &&
-					node.declarations[0].init.tag.name === importName
+					isCssTaggedTemplateLiteral(node, ecsstaticImportName)
 			) as VariableDeclaration[];
+
 			if (cssTemplateDeclarations?.length === 0) return;
+
+			const inlinedVars = findAllVariablesUsingEsbuild(id, {
+				parseFn: this.parse,
+				ecsstaticImportName,
+			});
 
 			cssTemplateDeclarations.forEach((node) => {
 				const originalName = (node.declarations[0].id as Identifier).name;
@@ -56,7 +61,10 @@ export const plugin = createUnplugin(() => {
 					templateContents += _quasi.value.raw;
 					if (index < quasi.quasis.length - 1) {
 						const expression = quasi.expressions[index];
-						templateContents += kindaEval(code.slice(expression.start, expression.end));
+						templateContents += evalWithVars(
+							code.slice(expression.start, expression.end),
+							inlinedVars
+						);
 					}
 				});
 
@@ -82,6 +90,10 @@ export const plugin = createUnplugin(() => {
 	};
 });
 
+/**
+ * processes template strings using postcss-nested and
+ * returns it along with a hashed classname based on the string contents.
+ */
 function processCss(templateContents = '', originalName = '') {
 	const className = `${originalName}-${hash(templateContents)}`;
 	const unprocessedCss = `.${className}{${templateContents}}`;
@@ -90,6 +102,7 @@ function processCss(templateContents = '', originalName = '') {
 	return [css, className];
 }
 
+/** parses ast and returns the name of the ecsstatic import and its start/end indexes */
 function processImport(ast: Program) {
 	let importName = '';
 
@@ -109,6 +122,60 @@ function processImport(ast: Program) {
 	return [importName, 0, 0] as const;
 }
 
-function kindaEval(expression: string, args = {}) {
-	return new Function(...Object.keys(args), `return ${expression}`)(...Object.values(args));
+/** uses Function() to evaluate expressions (inside tagged template literals) */
+function evalWithVars(expression: string, inlineVarDeclarations = '') {
+	return new Function(`${inlineVarDeclarations};\n return (${expression})`)();
+}
+
+/**
+ * uses esbuild to inline all imports, then returns all variable declarations.
+ * this can be passed to `evalWithVars` where inline expressions will be resolved correctly.
+ */
+function findAllVariablesUsingEsbuild(
+	fileId: string,
+	options: {
+		parseFn: (code: string) => unknown;
+		ecsstaticImportName?: string;
+	}
+) {
+	const { parseFn, ecsstaticImportName = 'css' } = options;
+
+	// this code will have all the imports inlined
+	const processedCode = esbuild.buildSync({
+		entryPoints: [fileId],
+		bundle: true,
+		format: 'esm',
+		write: false,
+		platform: 'node',
+		packages: 'external', // don't resolve packages
+	}).outputFiles[0].text;
+
+	const ast = parseFn(processedCode) as Program;
+
+	const varDeclarations = ast.body.filter(
+		(node) =>
+			node.type === 'VariableDeclaration' && !isCssTaggedTemplateLiteral(node, ecsstaticImportName)
+	) as VariableDeclaration[];
+
+	let returnValue = '';
+
+	varDeclarations.forEach(({ start, end }) => {
+		returnValue += processedCode.slice(start, end);
+	});
+
+	return returnValue;
+}
+
+/**
+ * true if a variable declaration matches this format:
+ * ```
+ * const x = css`...`;
+ * ```
+ */
+function isCssTaggedTemplateLiteral(node: VariableDeclaration, ecsstaticImportName = 'css') {
+	return (
+		node.declarations[0].init?.type === 'TaggedTemplateExpression' &&
+		node.declarations[0].init.tag.type === 'Identifier' &&
+		node.declarations[0].init.tag.name === ecsstaticImportName
+	);
 }
